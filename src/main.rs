f497104f8c89b614193dbcd05a6c9742f0c0439b
86189glr <86189@glr.nl>
2022-06-13 21:08:28 +0200

    final commit

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
-use std::{path, u8, vec};
+use std::{vec};
 
-use image::{open, DynamicImage, ImageBuffer, Luma, Rgb, RgbImage, RgbaImage};
+use image::{open, ImageBuffer, Luma};
 use rand::{thread_rng, Rng};
 
+const WIDTH: usize = 512;
 #[derive(Debug, Copy, Clone)]
 #[allow(non_snake_case)]
+
 struct Particle {
     posX: i16,
     posY: i16,
@@ -17,25 +19,24 @@ struct Particle {
 }
 
 impl Particle {
-    fn new(x: i16, y: i16, maxLifetime: i16) -> Particle {
+    fn new(x: i16, y: i16, max_life_time: i16) -> Particle {
         Particle {
             posX: x,
             posY: y,
             isDead: false,
-            lifetime: maxLifetime,
+            lifetime: max_life_time,
             caryCapacity: 0.0,
             carriedSendiment: 0.0,
             stuckcounter: 0,
-            maxLifetime: maxLifetime,
+            maxLifetime: max_life_time,
         }
     }
     #[allow(non_snake_case)]
-    fn moveParticle(
-        &mut self,
-        heightmap: &Vec<f32>,
-        buffer_erode: &mut Vec<f32>,
-        buffer_disposite: &mut Vec<f32>,
-    ) {
+
+    fn moveParticleRefactor(&mut self, heightmap: &Vec<f32>) -> [f32; 2] {
+        let mut erodeOutput: f32 = 0.0;
+        let mut dispositeOutput: f32 = 0.0;
+
         if self.posX - 1 > 2
             && self.posY - 1 > 2
             && self.posX - 1 < 510
@@ -44,79 +45,79 @@ impl Particle {
         {
             // find new position
 
-            let moveVector = self.findDirrection(heightmap);
+            let move_vector = self.findDirrection(heightmap);
 
             //typecast index of current position in array format
 
-            let posXu = self.posX as usize;
-            let posYu = self.posY as usize;
-            let posCord = posXu + (posYu * 512) as usize;
+            let pos_xusize = self.posX as usize;
+            let pos_yusize = self.posY as usize;
+            let pos_cord = pos_xusize + (pos_yusize * WIDTH) as usize;
 
             // typecast inded of new position
 
-            let addedCordsX = self.posX as i32 + moveVector[0] as i32;
-            let addedCordsY = self.posY as i32 + moveVector[1] as i32;
-            let newPosIndex = addedCordsX as usize + (addedCordsY * 512) as usize;
+            let added_cords_x = self.posX as i32 + move_vector[0] as i32;
+            let added_cords_y = self.posY as i32 + move_vector[1] as i32;
+            let new_pos_index = added_cords_x as usize + (added_cords_y * WIDTH as i32) as usize;
 
             //calculate the height diffrence and speed
-            let heightDiffrence = &heightmap[posCord] - &heightmap[newPosIndex];
-            let speed: f32 = (1.2 * 9.81 * heightDiffrence as f32).sqrt();
+            let height_diffrence = &heightmap[pos_cord] - &heightmap[new_pos_index];
+            let speed: f32 = (1.2 * 9.81 * height_diffrence as f32).sqrt();
 
             if speed <= 0.0 {
                 self.isDead = true;
-                buffer_disposite[posCord] += self.carriedSendiment;
+                dispositeOutput += self.carriedSendiment;
             }
 
             //calculate carry capactiy
 
             self.caryCapacity = (speed / (1.2 * 9.81 * 256 as f32).sqrt()
                 * (self.lifetime as f32 / self.maxLifetime as f32))
-                * 4.0;
+                * 30.0;
 
             //Add erroded away material to buffer
 
-            buffer_erode[posCord] += ((heightDiffrence as f32) * 0.2).min(heightDiffrence);
-            self.carriedSendiment += ((heightDiffrence as f32) * 0.2).min(heightDiffrence);
+            erodeOutput += ((height_diffrence as f32) * 0.2).min(height_diffrence);
+            self.carriedSendiment += ((height_diffrence as f32) * 0.2).min(height_diffrence);
 
             //disposite
 
             if self.carriedSendiment >= self.caryCapacity {
                 let diffrence = (self.caryCapacity - self.carriedSendiment).abs();
 
-                buffer_disposite[posCord] += &diffrence;
+                dispositeOutput += &diffrence;
                 self.carriedSendiment -= &diffrence;
             }
 
             //update position
 
-            if moveVector[0] == 0 && moveVector[0] == 0 {
+            if move_vector[0] == 0 && move_vector[0] == 0 {
                 self.stuckcounter += 1;
 
                 if self.stuckcounter > 10 {
                     self.isDead;
                     let posXu = self.posX as usize;
                     let posYu = self.posY as usize;
-                    let posCord = posXu + (posYu * 512) as usize;
-                    buffer_disposite[posCord] += self.carriedSendiment;
+                    let posCord = posXu + (posYu * WIDTH) as usize;
+                    dispositeOutput += self.carriedSendiment;
                 } else {
                     let mut rng = thread_rng();
-                    let rand_x = rng.gen_range(0..1);
-                    let rand_y = rng.gen_range(0..1);
+                    let rand_x = rng.gen_range(0..2);
+                    let rand_y = rng.gen_range(0..2);
 
                     self.posX += rand_x;
                     self.posY += rand_y;
                 }
             }
 
-            self.posX += moveVector[0];
-            self.posY += moveVector[1];
+            self.posX += move_vector[0];
+            self.posY += move_vector[1];
         } else {
             //ensure particle  dies.
             self.isDead = true;
             let posXu = self.posX as usize;
             let posYu = self.posY as usize;
-            let posCord = posXu + (posYu * 512) as usize;
-            buffer_disposite[posCord] += self.carriedSendiment;
+            let posCord = posXu + (posYu * WIDTH) as usize;
+            dispositeOutput += self.carriedSendiment;
         }
 
         //subtract lifetime
@@ -125,11 +126,13 @@ impl Particle {
             self.isDead = true;
             let posXu = self.posX as usize;
             let posYu = self.posY as usize;
-            let posCord = posXu + (posYu * 512) as usize;
-            buffer_disposite[posCord] += self.carriedSendiment;
+            let posCord = posXu + (posYu * WIDTH) as usize;
+            dispositeOutput += self.carriedSendiment;
         } else {
             self.lifetime -= 1;
         }
+
+        [erodeOutput, dispositeOutput]
     }
 
     #[allow(non_snake_case)]
@@ -143,8 +146,8 @@ impl Particle {
 
         for y in posYu - 1..posYu + 2 {
             for x in posXu - 1..posXu + 2 {
-                if &array[x + (512 * y)] <= &LowestPoint {
-                    LowestPoint = array[x + (512 * y)] as f32;
+                if &array[x + (WIDTH * y)] <= &LowestPoint {
+                    LowestPoint = array[x + (WIDTH * y)] as f32;
 
                     MoveVector[0] = x as i16;
                     MoveVector[1] = y as i16;
@@ -172,8 +175,6 @@ fn main() {
     let image_array = &img.unwrap().into_luma8().to_vec();
     let mut buffer_heightmap = vec![];
 
-    let mut Output:Vec<f32> = vec![];
-
     for (x, i) in image_array.iter().enumerate() {
         let value = image_array[x] as f32;
 
@@ -182,9 +183,8 @@ fn main() {
 
     //Create buffer
     // itterator
-    for _l in 0..300 {
-
-        let ammount_particles = 3000; //100000
+    for _l in 0..3000 {
+        let ammount_particles = 10000; //100000
         let mut Particle_List: Vec<Particle> = vec![];
         let mut buffer_erode: Vec<f32> = vec![];
         let mut buffer_disposite: Vec<f32> = vec![];
@@ -209,35 +209,42 @@ fn main() {
 
         //move particles
         for _z in 0..max_particle_lifetime {
-            let Particle_List_lenght = Particle_List.len();
+            let particle_list_lenght = Particle_List.len();
 
-            for x in 0..Particle_List_lenght {
+            for x in 0..particle_list_lenght {
                 if Particle_List[x].isDead == false {
-                    Particle_List[x].moveParticle(
-                        &buffer_heightmap,
-                        &mut buffer_erode,
-                        &mut buffer_disposite,
-                    );
+                    // Particle_List[x].moveParticle(
+                    //     &buffer_heightmap,
+                    //     &mut buffer_erode,
+                    //     &mut buffer_disposite,
+                    // );
+
+                    let output = Particle_List[x].moveParticleRefactor(&buffer_heightmap);
+                    let index_pos =
+                        (Particle_List[x].posX as usize) + (Particle_List[x].posY as usize * WIDTH);
+
+                    if index_pos < buffer_erode.len() {
+                        buffer_erode[index_pos] += output[0];
+                        buffer_disposite[index_pos] += output[1];
+                    }
+
+                    //println!("{:?}",output);
                 } else {
                     continue;
                 }
             }
         }
 
-        //  let sumDispote: f32 = buffer_disposite.iter().sum();
-        // let sumErode:f32 = buffer_erode.iter().sum();
 
         buffer_erode = blur_buffer(buffer_erode);
-
         buffer_disposite = blur_buffer(buffer_disposite);
 
         Particle_List.clear();
+
         for (x, i) in image_array.iter().enumerate() {
             buffer_heightmap[x] -= buffer_erode[x];
             buffer_heightmap[x] += buffer_disposite[x];
         }
-
-
 
         buffer_erode.clear();
         buffer_disposite.clear();
@@ -245,16 +252,14 @@ fn main() {
         // println!("sum disposite array:{} \n sum erode array {}",sumDispote,sumErode);
 
         println!("{}", _l);
-
-      
     }
 
-    let mut imageSave = ImageBuffer::new(512, 512);
+    let mut image_save = ImageBuffer::new(WIDTH as u32, WIDTH as u32);
 
-    for (x, y, pixel) in imageSave.enumerate_pixels_mut() {
-        let indexX = x as usize;
-        let indexY = y as usize;
-        let value = buffer_heightmap[indexX + (indexY * 512)];
+    for (x, y, pixel) in image_save.enumerate_pixels_mut() {
+        let index_x = x as usize;
+        let index_y = y as usize;
+        let value = buffer_heightmap[index_x + (index_y * WIDTH)];
 
         *pixel = image::Rgb([value as f32, value as f32, value as f32]);
     }
@@ -264,32 +269,13 @@ fn main() {
     fileName.push_str("end");
     fileName.push_str(format);
 
-    imageSave.save(fileName).unwrap();
+    image_save.save(fileName).unwrap();
 }
-
-// fn blur_buffer(array_to_blur:Vec<f32>)->Vec<f32>{
-
-//     let mut blur_buffer = ImageBuffer::new(512,512);
-
-//         for (x, y, pixel) in blur_buffer.enumerate_pixels_mut() {
-//             let index_x = x as usize;
-//             let index_y = y as usize;
-//             let value = array_to_blur[index_x + (index_y * 512)];
-
-//             *pixel = image::Rgb([value as f32, value as  f32, value as  f32]);
-
-//         }
-
-//         blur_buffer =  image::imageops::blur(&blur_buffer, 1.0);
-
-//    // blur_buffer.to_vec()
-
-// }
 
 fn blur_buffer(arr: Vec<f32>) -> Vec<f32> {
     image::imageops::blur(
-        &ImageBuffer::<Luma<f32>, Vec<f32>>::from_vec(512, 512, arr).unwrap(),
-        0.75,
+        &ImageBuffer::<Luma<f32>, Vec<f32>>::from_vec(WIDTH as u32, WIDTH as u32, arr).unwrap(),
+        2.0,
     )
     .to_vec()
 }
